747cdb34150484cbf8dc1c58ed758144fb73a1c1
Gor <7495500@gmail.com>
2022-10-11 15:54:28 +0300

    Add oop execises

---
+++ point.js
@@ -0,0 +1,44 @@
+function getX() {
+  return this.x;
+}
+
+function getY() {
+  return this.y;
+}
+
+export default function Point(x, y) {
+  this.x = x;
+  this.y = y;
+  this.getX = getX;
+  this.getY = getY;
+}
+
+
+
+
+function getBeginPoint() {
+  return this.beginPoint;
+}
+
+function getEndPoint() {
+  return this.endPoint;
+}
+
+export default function Segment(beginPoint, endPoint) {
+  this.beginPoint = beginPoint;
+  this.endPoint = endPoint;
+  this.getBeginPoint = getBeginPoint;
+  this.getEndPoint = getEndPoint;
+}
+
+
+
+
+export default (segment) => {
+  const beginPoint = segment.getBeginPoint();
+  const endPoint = segment.getEndPoint();
+  const newEndPoint = new Point(beginPoint.getX(), beginPoint.getY());
+  const newBeginPoint = new Point(endPoint.getX(), endPoint.getY());
+
+  return new Segment(newBeginPoint, newEndPoint);
+};
