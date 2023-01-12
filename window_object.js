c24e1923fa57fa2d9af3735c5a5f2ce6bc53013e
Gor <7495500@gmail.com>
2023-01-12 21:28:01 +0300

    Working with wondow.object example

---
+++ window_object.js
@@ -0,0 +1,15 @@
+if (window.confirm("Push OK or CANCEL")) {
+  console.log("You pushed OK");
+} else {
+  console.log("You pushed CANCEL");
+}
+
+window.alert('hey')
+
+var UNname = window.prompt("What is your name?", "NoName");
+
+if (UNname == null) {
+  alert("Bye!");
+} else {
+  document.write("Hey " + UNname);
+}
