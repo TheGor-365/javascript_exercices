8f0416ce9201ca0cf9b734d5b75526844dfaa3d0
Gor <7495500@gmail.com>
2023-10-03 14:06:51 +0300

    Modify date.js

--- date.js
+++ date.js
@@ -9,3 +9,4 @@ console.log(date.getFullYear());
 console.log(date.getHours());
 console.log(date.getMilliseconds());
 console.log(date.valueOf().toString() + date.getMilliseconds().toString());
+console.log(date.getTime());
