7d97af2f3345026fb36224ac4377b686d3003332
Gor <7495500@gmail.com>
2023-10-23 12:53:45 +0300

    Add shouldBeBig.js

---
+++ shouldBeBig.js
@@ -0,0 +1,10 @@
+export default (str) => {
+  let result = '';
+  
+  for (let i = 0; i < length(str); i += 1) {
+    const shouldBeBig = (i === 0 || str[i - 1] === ' ');
+    result += shouldBeBig ? toUpperCase(str[i]) : str[i];
+  }
+
+  return result;
+};
