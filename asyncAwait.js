75eef89d0428d2636c942c057860a06c733eaf17
Gor <7495500@gmail.com>
2023-10-28 15:54:20 +0300

    Add async await ex

---
+++ asyncAwait.js
@@ -0,0 +1,8 @@
+export const exchange = async (filepath1, filepath2) => {
+  const reading1 = fsp.readFile(filepath1);
+  const reading2 = fsp.readFile(filepath2);
+  const [data1, data2] = await Promise.all([reading1, reading2]);
+  const writing1 = fsp.writeFile(filepath1, data2);
+  const writing2 = fsp.writeFile(filepath2, data1);
+  await Promise.all([writing1, writing2]);
+};
